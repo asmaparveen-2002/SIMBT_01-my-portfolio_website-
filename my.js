d128b6627a7557f21df32dd6acf76a82ee124217
asmaparveen-2002 <aasmaparveen1509@gmail.com>
2023-04-01 02:23:37 +0530

    a

---
+++ my.js
@@ -0,0 +1,16 @@
+function userinput(){
+    let username=document.getElementById('name').value
+    let number=document.getElementById("number").value
+    let email=document.getElementById("email").value
+    alert("Your Form is submitted Succefully \n \n"+"your name is : "+ username + "\n your Email is : " + email +"\n your contact number is : " + number+"\n" )
+
+    
+}
+
+// function add(){
+
+//     let a=2;
+//     let b=3;
+//     return ;
+// }
+
